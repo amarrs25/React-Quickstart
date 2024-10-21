078491e60551a982def3944818298ede8c72e5ef
amarrs25 <aaron.marrs44@gmail.com>
2024-10-21 16:11:50 -0500

    Using Hooks & Sharing data between components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -70,19 +70,39 @@ import './App.css';
 
 //Responding to events & Updating the screen
   //Counters that count independently
-import { useState } from 'react';
+// import { useState } from 'react';
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Counters that update separately</h1>
+//       <MyButton />
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
+// ----------------------------------------
+
+//Using Hooks & Sharing data between components
+  //Counters that update together
+
+  import { useState } from 'react';
 
 export default function MyApp() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
-  );
-}
-
-function MyButton() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
@@ -90,9 +110,20 @@ function MyButton() {
   }
 
   return (
-    <button onClick={handleClick}>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
       Clicked {count} times
     </button>
   );
 }
+
 // ----------------------------------------
